6576b40fd4bed2d499cb6070806fe7777dc95b53
Alex Pererodov <pererodovsasha2362000@gmail.com>
2025-10-19 00:03:49 +0300

    Refactor code structure for improved readability and maintainability

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+window.addEventListener("DOMContentLoaded", () => {
+	function burgerMenu() {
+		const header = document.querySelector(".header");
+		const buttonBurger = document.querySelector(".burger-menu");
+
+		buttonBurger.addEventListener("click", () => {
+			if (header.classList.contains("_active")) {
+				header.classList.remove("_active");
+				document.body.style.overflow = "";
+			} else {
+				header.classList.add("_active");
+				document.body.style.overflow = "hidden";
+			}
+		});
+	}
+
+
+	burgerMenu();
+})
